72d0a23da73fdae83470bc40150375d01125c80e
Geoffrey Okongo <geoffreyokongo2@gmail.com>
2016-01-05 15:36:16 +0300

    Removing the menu link from the menu bar

--- application/views/videos/menu-bar.glade.php
+++ application/views/videos/menu-bar.glade.php
@@ -3,12 +3,11 @@
   <div class="col-md-12">
         <div class="navbar-header">
           
-          <a href="{{Url::base()}}" class="navbar-brand">Home</a>
+          <a href="{{Url::link('videos')}}" class="navbar-brand ">Back</a>
       
         </div>
         <div class="collapse navbar-collapse" id="navbar-collapse1">
           <ul class="nav navbar-nav navbar-right">
-             <li><a href="{{Url::link('videos')}}" class="lead">Back</a></li>
              <li><a href="" class="lead">Reload Page</a></li>
            </ul>
         </div>  
