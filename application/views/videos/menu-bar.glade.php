d1bcd5e211270baefe31c145bf19d32c71d89d12
Geoffrey Okongo <geoffreyokongo2@gmail.com>
2016-01-05 14:06:45 +0300

    Adding the menu-bar file

---
+++ application/views/videos/menu-bar.glade.php
@@ -0,0 +1,16 @@
+
+<nav class="navbar navbar-fixed-top header">
+  <div class="col-md-12">
+        <div class="navbar-header">
+          
+          <a href="{{Url::base()}}" class="navbar-brand">Home</a>
+      
+        </div>
+        <div class="collapse navbar-collapse" id="navbar-collapse1">
+          <ul class="nav navbar-nav navbar-right">
+             <li><a href="{{Url::link('videos')}}" class="lead">Back</a></li>
+             <li><a href="" class="lead">Reload Page</a></li>
+           </ul>
+        </div>  
+     </div> 
+</nav>
