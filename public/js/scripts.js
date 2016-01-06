17830506dda5489226dd498d83651f78f7a975c0
Geoffrey Okongo <geoffreyokongo2@gmail.com>
2016-01-06 13:19:16 +0300

    Writing javascript for fetching video thumbnails

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -11,3 +11,24 @@ $('#btnToggle').click(function(){
   }
 });
 });
+
+$(document).ready(function(){
+
+	$("div.container").on('submit','form#add_new_video',  function(){
+
+		var video_src       = $('#video_url').val();
+
+		var video_id = video_src.match(/youtube\.com.*(\?v=|\/embed\/)(.{11})/).pop();
+
+		if (video_id.length == 11) {
+
+			//set the first thumbnail value
+			$('#thumbnail_1').val('http://img.youtube.com/vi/'+video_id+'/0.jpg');
+			$('#thumbnail_2').val('http://img.youtube.com/vi/'+video_id+'/1.jpg');
+			$('#thumbnail_3').val('http://img.youtube.com/vi/'+video_id+'/2.jpg');
+
+		}
+		 
+	});
+
+});
