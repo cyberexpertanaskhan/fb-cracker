dc4cf4d32402867d079556aec4dc01eff41f7505
Geoffrey Okongo <geoffreyokongo2@gmail.com>
2016-01-06 13:20:21 +0300

    Adding input fields for storing the image thumbnail urls

--- application/views/videos/edit.glade.php
+++ application/views/videos/edit.glade.php
@@ -19,11 +19,15 @@
 
         	@foreach($video as $videoInfo)
 
-            <form method="post" action="{{Url::link('videos/edit')}}">
+            <form id="add_new_video" method="post" action="{{Url::link('videos/edit')}}">
             	<input type="hidden" name="id" class="hidden" value="{{$videoInfo->id}}" />
 
+                <input name="thumbnail_1" id="thumbnail_1" value="" type="hidden" />
+                <input name="thumbnail_2" id="thumbnail_2" value="" type="hidden" />
+                <input name="thumbnail_3" id="thumbnail_3" value="" type="hidden" />
+
                 <div class="col-md-12"><br><br> <input name="video_name" type="text" class="form-control" placeholder="Name" value="{{$videoInfo->name}}" required/></div>
-                <div class="col-md-12"><br><br> <input name="video_url" type="url" class="form-control" placeholder="Url"  value="{{$videoInfo->url}}" required/></div>
+                <div class="col-md-12"><br><br> <input name="video_url" id="video_url" type="url" class="form-control" placeholder="Url"  value="{{$videoInfo->url}}" required/></div>
                 <div class="col-md-12"><br><br> <textarea class="form-control" name="video_description" placeholder="Description">{{$videoInfo->description}}</textarea></div>
                 <div class="col-md-12"><br><br> <input name="video_comments" type="text" class="form-control" placeholder="Comments" value="{{$videoInfo->comments}}"/></div>
                 <div class="col-md-12"><br><br> <button name="submit" type="submit" class="btn btn-success"/>Update</button></div>
